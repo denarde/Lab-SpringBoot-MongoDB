bd957b83987112031754a1eec01ea64aeb61e749
denarde <luiz_denarde@hotmail.com>
2022-08-20 17:04:57 -0300

    Finish Project | Update README

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # HandsOnSpringMongoDB
 Hands-on API Rest com Spring Boot utilizando MongoDB para persistencia de dados.
+
+**Endpoints :**
+* POST   > api/contato
+* PUT    > api/contato/{id}
+* GET    > api/contato
+* DELETE > api/contato/{id}
+
+**Porta Default :** 8080
+
+**Subir o banco :**
+
+ˋdocker-compose up -dˋ
--- src/main/java/com/denarde/agenda/api/controller/ContatoController.java
+++ src/main/java/com/denarde/agenda/api/controller/ContatoController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/contato")
 public class ContatoController {
@@ -15,7 +17,34 @@ public class ContatoController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    Contato save(@RequestBody Contato contato){
+    Contato save(@RequestBody Contato contato) {
         return contatoRepository.save(contato);
     }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void update(@PathVariable String id, @RequestBody Contato contato) {
+        contatoRepository.findById(id)
+                .map(c -> {
+                    contato.setId(c.getId());
+                    contatoRepository.save(contato);
+                    return contato;
+                }).orElseThrow(() -> new RuntimeException("Contato não encontrado"));
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void delete(@PathVariable String id){
+        contatoRepository.findById(id)
+                .map(c -> {
+                    contatoRepository.delete(c);
+                    return Void.TYPE;
+                }).orElseThrow(() -> new RuntimeException("Contato não encontrado"));
+    }
+
+    @GetMapping
+    List<Contato> getAll() {
+        return contatoRepository.findAll();
+    }
+
 }
